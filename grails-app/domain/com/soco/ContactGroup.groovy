4c8cabed530d0838a3d08d348a6e6705b1dc775b
JohnWU <wjmail@gmail.com>
2015-05-06 13:29:00 +0800

    add contact group

---
+++ grails-app/domain/com/soco/ContactGroup.groovy
@@ -0,0 +1,12 @@
+package com.soco
+
+class ContactGroup {
+
+	String name;
+	String nick_name;
+	String type;
+	String description;
+	
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/com/soco/UserContactGroup.groovy
@@ -0,0 +1,10 @@
+package com.soco
+
+class UserContactGroup {
+	
+	long contact_group_id;
+	long user_id;
+
+    static constraints = {
+    }
+}
